9b0ea65ec520950723d1af52dcec0822a38f2919
Abhimanyu-Samantaray <ass.samantaray@gmail.com>
2026-06-20 17:42:29 +0530

    About page Design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,24 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { IndexComponent } from './components/index/index.component';
+import { ProjectComponent } from './components/project/project.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: IndexComponent
+    },
     {
         path: 'about',
         component: AboutComponent
     },
     {
-        path: '',
-        component: IndexComponent
+        path: 'project',
+        component: ProjectComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
     }
 ];
